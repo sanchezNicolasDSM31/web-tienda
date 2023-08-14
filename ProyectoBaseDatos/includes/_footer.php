ebe9fd7efac1c6e534e8ac5e88ff0190439dd87a
sanchezNicolasDSM31 <al222210895@gmail.com>
2023-08-13 20:57:15 -0600

    Primer commit

---
+++ ProyectoBaseDatos/includes/_footer.php
@@ -0,0 +1,57 @@
+<footer class="text-center text-lg-start bg-dark text-muted">
+  <section
+    class="d-flex justify-content-center justify-content-lg-between p-4  border-bottom">
+
+    <div class="me-3 d-none d-lg-block">
+      <span>Siguenos en nuestras redes sociales:</span>
+    </div>
+        <a href="https://www.facebook.com/ian.cordoba.944/" class="me-4 text-reset">
+          <i class="bi bi-facebook"></i>
+        </a>
+        <a href="https://twitter.com/yung_ask" class="me-4 text-reset">
+          <i class="bi bi-twitter"></i>
+        </a>
+        <a href="https://www.youtube.com/@yungask8400" class="me-4 text-reset">
+          <i class="bi bi-youtube"></i>
+        </a>
+        <a href="https://www.instagram.com/iyungask/" class="me-4 text-reset">
+          <i class="bi bi-instagram"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+          <i class="bi bi-linkedin"></i>
+        </a>
+    </div>
+  </section>
+    <div class="container text-center text-md-start mt- bg-dark">
+      <div class="row mt-3">
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3"></i>!Conocenos¡
+          </h6>
+          <p>
+          La aplicación permite a los usuarios agregar y registrar información detallada sobre cada producto, incluyendo su nombre, descripción, categoría, precio, cantidad,cantidad minima y ubicación física.
+           Esta base de datos centralizada garantiza que todos los datos estén al alcance y actualizados en todo momento....
+          </p>
+        </div>
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <h6 class="text-uppercase fw-bold mb-4">
+            Contactanos en:
+          </h6>
+          <p><i class="fas fa-home me-3"></i> Lerma ,Estado de Mexico, 52004</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            nsanchezalmeyda@gmail.com
+          </p>
+          <p><i class="fas fa-phone me-3"></i> +52 722-344-9081</p>
+        </div>
+      </div>
+    </div>
+    
+  <div class="text-center bd-dark">
+    © 2023 Copyright: UTVT-EQUIPO-5
+  </div>
+  </section>
+
+
+</footer>
+  </body>
